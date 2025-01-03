dfe69f9de05288bd0229b92ecef69127d1b29033
Kristaps Grava <kristap.grava@gmail.com>
2025-01-03 12:43:36 +0200

    added debugging comments

--- main.py
+++ main.py
@@ -187,9 +187,11 @@ for instruction in instructions:
     #a check to see for which device the instruction is written; then the instruction is sent
     if instruction.startswith('WIRE' or 'SOLENOID'):
         #main.sendInstruction(instruction)
+        print('shouldnttt be sent????wtf')
         pass
 
     elif instruction.startswith('G1' or 'G4' or 'M30' or 'F'):
+        print("senttt!! success")
         stepper.sendInstruction(instruction)
 
     elif instruction.startswith('PEO'):
